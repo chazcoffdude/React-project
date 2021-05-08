6d5a03a35ee3c4197f550923a0d33de844d577e2
chazcoffdude <71471786+chazcoffdude@users.noreply.github.com>
2021-05-07 18:18:34 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import React from 'react';
+
+
+class App extends React.Component {
+  render(){
+    return <h1>Hi how are you</h1>
+  }
+}
+//extends out a class
+export default App;
+//trying out modifying the App.js as instructed by video
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+//file created to be new component
+
+ const Header = () => {
+    return (
+       <header>Task tracker</header>
+    )
+}
+export default Header
+//code to export component in order for it to be usable
